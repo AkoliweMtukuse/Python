5502c91be670a697ecbbaaacf137dee0e1aa0743
AkoliweMtukuse <61545835+AkoliweMtukuse@users.noreply.github.com>
2020-03-03 01:39:18 -0800

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,13 +1,11 @@
-i=1
-while i <= 100:
-    if i%3==0:
-        print("Fizz", end="")
-        if i%5==0:
-            print("Buzz", end="")
+for i in range(101):
+    if i==0:
+        continue
+    elif i%15==0:
+        print 'FizzBuzz'
     elif i%5==0:
-        print("Buzz", end="")
+        print 'Buzz'
+    elif i%3==0:
+        print 'Fizz'
     else:
-        print(i, end="")
-    print()
-    i+=1
-
+        print i
